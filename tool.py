4fa2fe4d5d0fc4a3cd60950e122d418eec6f44b3
Absoler-in-seclab_zkb-csmith <1045607979@qq.com>
2023-12-02 21:22:00 +0800

    support rough check compiler's trigger ratio, compatible with old version of python3

--- tool.py
+++ tool.py
@@ -4,14 +4,20 @@ import re
 import os
 import sys
 
+opt_option=
+test_type=
+pin_root=
+root_dir=
+
 parser = argparse.ArgumentParser(description="some practicals functions for test")
 
 parser.add_argument("--file_range", default="" ,type=str, help="fileNo range")
 parser.add_argument("-d", default="./", type=str, help="target directory")
-
+parser.add_argument("--generate", "-gen", default=-1, help="only generate [NUM] test cases in ./caserepo", type=int)
+parser.add_argument("--test", default="", help="specify compiler, count number of triggered case, use -gen to specify test range")
 
 def get_file_range(range:str, dir:str):
-    res:list[str] = []
+    res = []
     grp = re.match(r"(\d*)-(\d*)", range)
     start, end = map(int, [
         grp[1] if grp[1] != "" else "0",
@@ -20,7 +26,7 @@ def get_file_range(range:str, dir:str):
     files = os.listdir(dir)
     for file in files:
         if start <= int(re.match(r"(\d+).*", file)[1]) < end:
-            res.append(f'{dir}/{file}')
+            res.append(dir + "/" + file)
     return res
 
 
@@ -31,3 +37,41 @@ if __name__ == "__main__":
         files = get_file_range(args.file_range, directory)
         for file in files:
             print(file)
+
+    gen = int(args.generate)
+    if gen != -1 and args.test == "":
+        if not os.path.exists("./caserepo"):
+            os.mkdir("./caserepo")
+        for i in range(gen):
+            os.system("build/src/csmith --copyPropagation --no-safe-math --no-bitfields --no-volatiles --probability-configuration ./prob.txt  -o caserepo/output{}.c 1>/dev/null".format(i))
+            if i % 1000 == 0:
+                print("generate {} cases".format(i))
+    
+    if args.test:
+        assert(gen != -1)
+        compiler = args.test
+        if not os.path.exists("./caserepo"):
+            print("ERROR: no ./caserepo directory", file=sys.stderr)
+        
+        triggercnt = 0
+        respath = compiler + ".res"
+
+        oldpath = os.getcwd()
+        tempdir = os.popen("mktemp -d").read().strip()
+        os.chdir(tempdir)
+        for i in range(gen):
+            casepath = "{}/output{}.c".format(tempdir, i)
+            os.system("cp {}/caserepo/output{}.c {}".format(root_dir, i, casepath))
+            if not os.path.exists(casepath):
+                print("ERROR: missing " + casepath, file=sys.stderr)
+            os.system("{} {} {} 2>/dev/null".format(compiler, casepath, opt_option))
+            ret = os.system("timeout -s SIGTERM 5s {}/pin -t {}/checkAccess/obj-intel64/checkAccess.so -- ./a.out {} func ./ 1>/dev/null".format(pin_root, root_dir, test_type))
+            res = os.popen("cat result.out").read().strip()
+            if ret == 0 and res == "1":
+                triggercnt += 1
+                with open(respath, "a") as f:
+                    f.write("{} trigger {}\n".format(compiler, i))
+        print("{} / {} triggered".format(triggercnt, gen))
+        os.chdir(oldpath)
+        os.system("rm {} -r".format(tempdir))
+        print(tempdir)
